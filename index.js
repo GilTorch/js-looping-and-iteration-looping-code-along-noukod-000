b14915d0b932245558c55eede57599b401e9cb09
Gilbert Torchon <gilbert@noukod.com>
2019-02-16 17:52:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ function printBadges(array){
 function tailsNeverFails(){
   let r=Math.random()>=0.5
   let tailCount=0
-  while(r){
-    tailCount++
-  }
+
   return `You got ${tailCount} tails in a row!`
 }
